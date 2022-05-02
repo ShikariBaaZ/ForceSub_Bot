31383d99f0a7145d05bc3ffd316a219f531aed57
Lawless_Shikari <91068760+ShikariBaaZ@users.noreply.github.com>
2022-05-02 15:07:30 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,151 @@
+#    This file is a part of the ForceSub distribution (https://github.com/ShikariBaaZ/ForceSub_Bot).
+#    Copyright (c) 2021 Shikari
+#    
+#    This program is free software: you can redistribute it and/or modify  
+#    it under the terms of the GNU General Public License as published by  
+#    the Free Software Foundation, version 3.
+# 
+#    This program is distributed in the hope that it will be useful, but 
+#    WITHOUT ANY WARRANTY; without even the implied warranty of 
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+#    General Public License for more details.
+# 
+#    License can be found in < https://github.com/ShikariBaaZ/ForceSub_Bot> .
+
+import logging
+import asyncio
+import string
+import random
+from telethon.utils import get_display_name
+import re
+from telethon import TelegramClient, events, Button
+from decouple import config
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s', level=logging.INFO)
+
+appid = apihash = bottoken = None
+# start the bot
+print("Starting...")
+try:
+    apiid = config("API_ID", cast=int)
+    apihash = config("API_HASH")
+    bottoken = config("BOT_TOKEN")
+    xchannel = config("CHANNEL")
+    welcome_msg = config("WELCOME_MSG")
+    welcome_not_joined = config("WELCOME_NOT_JOINED")
+    on_join = config("ON_JOIN", cast=bool)
+    on_new_msg = config("ON_NEW_MSG", cast=bool)
+except:
+    print("Environment vars are missing! Kindly recheck.")
+    print("Bot is quiting...")
+    exit()
+
+if (apiid != None and apihash!= None and bottoken != None):
+    try:
+        Shikari = TelegramClient('Shikari', apiid, apihash).start(bot_token=bottoken)
+    except Exception as e:
+        print(f"ERROR!\n{str(e)}")
+        print("Bot is quiting...")
+        exit()
+else:
+    print("Environment vars are missing! Kindly recheck.")
+    print("Bot is quiting...")
+    exit()
+
+channel = xchannel.replace("@", "")
+
+# join check
+async def get_user_join(id):
+    ok = True
+    try:
+        await Shikari(GetParticipantRequest(channel=channel, participant=id))
+        ok = True
+    except UserNotParticipantError:
+        ok = False
+    return ok
+
+
+@Shikari.on(events.ChatAction())
+async def _(event):
+    if on_join is False:
+        return
+    if event.user_joined or event.user_added:
+        user = await event.get_user()
+        chat = await event.get_chat()
+        title = chat.title if chat.title else "this chat"
+        pp = await Shikari.get_participants(chat)
+        count = len(pp)
+        mention = f"[{get_display_name(user)}](tg://user?id={user.id})"
+        name = user.first_name
+        last = user.last_name
+        if last:
+            fullname = f"{name} {last}"
+        else:
+            fullname = name
+        uu = user.username
+        if uu:
+            username = f"@{uu}"
+        else:
+            username = mention
+        x = await get_user_join(user.id)
+        if x is True:
+            msg = welcome_msg.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
+            butt = [Button.url("Channel", url=f"https://t.me/{channel}")]
+        else:
+            msg = welcome_not_joined.format(mention=mention, title=title, fullname=fullname, username=username, name=name, last=last, channel=f"@{channel}")
+            butt = [Button.url("Channel", url=f"https://t.me/{channel}"), Button.inline("UnMute Me", data=f"unmute_{user.id}")]
+            await Shikari.edit_permissions(event.chat.id, user.id, until_date=None, send_messages=False)
+        
+        await event.reply(msg, buttons=butt)
+
+
+@Shikari.on(events.NewMessage(incoming=True))
+async def mute_on_msg(event):
+    if event.is_private:
+        return
+    if on_new_msg is False:
+        return
+    x = await get_user_join(event.sender_id)
+    temp = await Shikari(GetFullUserRequest(event.sender_id))
+    if x is False:
+        if temp.user.bot:
+            return
+        nm = temp.user.first_name
+        try:
+            await Shikari.edit_permissions(event.chat.id, event.sender_id, until_date=None, send_messages=False)
+        except Exception as e:
+            print(str(e))
+            return
+        await event.reply(f"Hey {nm}, seems like you haven't joined our channel. Please join @{channel} and then press the button below to unmute yourself!", buttons=[[Button.url("Channel", url=f"https://t.me/{channel}")], [Button.inline("UnMute Me", data=f"unmute_{event.sender_id}")]])
+
+
+@Shikari.on(events.callbackquery.CallbackQuery(data=re.compile(b"unmute_(.*)")))
+async def _(event):
+    uid = int(event.data_match.group(1).decode("UTF-8"))
+    if uid == event.sender_id:
+        x = await get_user_join(uid)
+        nm = (await Shikari(GetFullUserRequest(uid))).user.first_name
+        if x is False:
+            await event.answer(f"You haven't joined @{channel} yet!", cache_time=0, alert=True)
+        elif x is True:
+            try:
+                await Shikari.edit_permissions(event.chat.id, uid, until_date=None, send_messages=True)
+            except Exception as e:
+                print(str(e))
+                return
+            msg = f"Welcome to {(await event.get_chat()).title}, {nm}!\nGood to see you here!"
+            butt = [Button.url("Channel", url=f"https://t.me/{channel}")]
+            await event.edit(msg, buttons=butt)
+    else:
+        await event.answer("You are an old member and can speak freely! This isn't for you!", cache_time=0, alert=True)
+
+@Shikari.on(events.NewMessage(pattern="/start"))
+async def strt(event):
+    await event.reply(f"Hey❤️. I'm a 💐force subscribe bot💐 made specially for ❤️@{channel}!\n\nCheckout ❤️@The_SHIKARI_Network :)", buttons=[Button.url("🎯CHANNEL🎯", url=f"https://t.me/{channel}"), Button.url("💐REPOSITORY💐", url="https://github.com/ShikariBaaZ/ForceSub_Bot")])
+
+    
+print("ForceSub Bot has started.\nDo visit @The_SHIKARI_Network!")
+Shikari.run_until_disconnected()
